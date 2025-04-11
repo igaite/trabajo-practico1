10185856f8cc4c4dcce30ff3f1406b821cab749b
igaite <iairgaite@gmail.com>
2025-04-10 21:22:39 -0300

    Parte 1: Acceso a elementos y recorrido de arrays

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-// Main
+const productos = [
+    { id: 1, nombre: "Remera", precio: 1500, categoria: "Ropa" },
+    { id: 2, nombre: "Pantalón", precio: 2500, categoria: "Ropa" },
+    { id: 3, nombre: "Zapatillas", precio: 8000, categoria: "Calzado" }, { id: 4, nombre: "Gorra", precio: 1200, categoria: "Accesorios" },
+    { id: 5, nombre: "Campera", precio: 9500, categoria: "Ropa" }
+    ];
+
+// Parte 1
+// 1.
+console.log(productos[0].nombre);
+// 2.
+for (const producto of productos) {
+    console.log(`Producto: ${producto.nombre} - Precio: $${producto.precio}`);
+}
+//3.
+productos.forEach(producto => {
+    console.log(`Producto: ${producto.nombre} - Precio: $${producto.precio}`);
+});
